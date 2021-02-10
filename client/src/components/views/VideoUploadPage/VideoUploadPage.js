367d7dfeb826955a0f7502de0356bcf8634915e0
DESKTOP-I8S375Q\kunhee <tha920@naver.com>
2021-02-11 06:15:33 +0900

    비디오 업로드 페이지 생성 및 비디오 업로드, 썸네일 보여주기 기능 구현완료
    -> fluent-ffmpeg, multer, react-dropzone 라이브러리 추가

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
+import Axios from "axios";
 
 const { TextArea } = Input;
 const { Title } = Typography;
 
+const PrivateOptions = [
+  {
+    value: 0,
+    label: "Private",
+  },
+  { value: 1, label: "Public" },
+];
+
+const CategoryOptions = [
+  { value: 0, label: "Film & Animation" },
+  { value: 1, label: "Autos & Vehicle" },
+  { value: 2, label: "Music" },
+  { value: 3, label: "Pets & Animals" },
+];
+
 const VideoUploadPage = () => {
+  const [VideoTitle, setVideoTitle] = useState("");
+  const [Description, setDescription] = useState("");
+  const [Private, setPrivate] = useState(0);
+  const [Category, setCategory] = useState("Film & Animation");
+  const [FilePath, setFilePath] = useState("");
+  const [Duration, setDuration] = useState("");
+  const [ThumbnailPath, setThumbnailPath] = useState("");
+
+  const onTitleChange = (e) => {
+    setVideoTitle(e.currentTarget.value);
+  };
+
+  const onDescriptionChange = (e) => {
+    setDescription(e.currentTarget.value);
+  };
+
+  const onPrivateChange = (e) => {
+    setPrivate(e.currentTarget.value);
+  };
+
+  const onCategoryChange = (e) => {
+    setCategory(e.currentTarget.value);
+  };
+
+  const onDrop = (files) => {
+    let formData = new FormData();
+
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+    formData.append("file", files[0]);
+    Axios.post("/api/video/uploadfiles", formData, config).then((response) => {
+      if (response.data.success) {
+        // console.log(response.data);
+
+        let variable = {
+          url: response.data.url,
+          fileName: response.data.fileName,
+        };
+
+        setFilePath(response.data.url);
+
+        Axios.post("/api/video/thumbnail", variable).then((response) => {
+          if (response.data.success) {
+            setDuration(response.data.fileDuration);
+            setThumbnailPath(response.data.url);
+          } else {
+            alert("썸네일 생성에 실패 했습니다.");
+          }
+        });
+      } else {
+        alert("비디오 업로드를 실패했습니다.");
+      }
+    });
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
@@ -15,7 +87,7 @@ const VideoUploadPage = () => {
       <Form onSubmit>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {/* drop zone */}
-          <Dropzone onDrop multiple maxSize>
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={1000000000}>
             {({ getRootProps, getInputProps }) => (
               <div
                 style={{
@@ -35,28 +107,44 @@ const VideoUploadPage = () => {
           </Dropzone>
 
           {/* thumbnail */}
-          <div>
-            <img src="" alt="" />
-          </div>
+          {ThumbnailPath && (
+            <div>
+              <img
+                src={`http://localhost:5000/${ThumbnailPath}`}
+                alt="thumbnail"
+              />
+            </div>
+          )}
         </div>
         <br />
         <br />
+
         <label>Title</label>
-        <Input onChange value />
+        <Input onChange={onTitleChange} value={VideoTitle} />
         <br />
         <br />
+
         <label htmlFor="">Description</label>
-        <TextArea />
+        <TextArea onChange={onDescriptionChange} value={Description} />
         <br />
         <br />
-        <select onChange>
-          <option key value></option>
+
+        <select onChange={onPrivateChange}>
+          {PrivateOptions.map((item, idx) => (
+            <option key={idx} value={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
         <br />
         <br />
 
-        <select onChange>
-          <option key value></option>
+        <select onChange={onCategoryChange}>
+          {CategoryOptions.map((item, idx) => (
+            <option key={idx} value={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
         <br />
         <br />
